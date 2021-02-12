0354d258ab337cd04607b28c956b8c9e0e53752b
Ram <ramvilash.mi@gmail.com>
2021-02-12 18:56:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+import React,{ Fragment,useState} from 'react';
+import { BrowserRouter  as Router,Switch,Route} from  'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from "./components/pages/About";
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
+import NotFound from "./components/pages/NotFound";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import reactDom from 'react-dom';
+
+const  App =()=> {
+
+  // const [users,setUsers]=useState([]);
+  // const [user,setUser]=useState({});
+  // const [repos,setRepos]=useState([]);
+  // const [loading,setLoading]=useState(false);
+  // const [alert,showAlert]=useState(null);
+
+  // state={
+  //   users:[],
+  //   user:{},
+  //   repos:[],
+  //   loading:false,
+  //   alert:null
+  // }
+ 
+//  async componentDidMount(){
+//     // axios
+//     //   .get('https://api.github.com/users')
+//     //   .then(res=>console.log(res.data))
+//     this.setState({loading:true});
+
+
+//     const res= await axios.get(`https://api.github.com/users?client_id=
+//     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+//     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+//     this.setState({users:res.data,loading:false});
+//     console.log(res.data);
+//   }
+
+  // const searchUsers=async text =>{
+
+  //   setLoading(true);
+
+
+  //   const res= await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   setUsers(res.data.items);
+  //   setLoading(false);
+  // }
+
+
+  // //Get single User
+  // const getUser=async (username)=>{
+  //   setLoading(true);
+
+
+  //   const res= await axios.get(`https://api.github.com/users/${username}?client_id=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   setUser(res.data);
+  //   setLoading(false);
+
+  // }
+
+  //  //Get User Repos
+  //  const getUserRepos=async (username)=>{
+  //   setLoading(true);
+
+
+  //   const res= await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   setRepos(res.data);
+  //   setLoading(false);
+
+  // }
+  
+
+
+//  const clearUsers=()=>{
+//     setUsers([]);
+//     setLoading(false);
+//  } 
+
+//  const  setAlert =(msg,type)=>{
+//       showAlert({msg,type});
+
+//       setTimeout(()=>setAlert(null),5000);
+//   }
+
+  
+    return (
+      <GithubState>
+        <AlertState>
+          <Router>
+            <div>
+              <Navbar />
+              <div className="container">
+                <Alert alert={alert} />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/user/:login" component={User} />
+                  <Route component={NotFound} />
+                </Switch>
+              </div>
+            </div>
+          </Router>
+          </AlertState>
+        </GithubState>
+      
+    );
+ 
+ 
 }
 
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from 'react';
+import Search from '../../components/users/Search';
+import Users from '../../components/users/Users';
+
+ const Home = () => {
+    return (
+        <Fragment>
+        <Search />
+
+        <Users />
+      </Fragment>        
+    )
+}
+
+export default Home;
